5614e17851838ec1234b0733c6390269b01b3c0d
vinelimaa <vinicius.damas@hotmail.com>
2023-02-23 14:46:19 -0300

    restfull web services example

---
+++ restfull_web_services/src/main/java/com/in28minutes/rest_webservices/restfull_web_services/user/UserDaoService.java
@@ -0,0 +1,47 @@
+package com.in28minutes.rest_webservices.restfull_web_services.user;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDaoService {
+
+	// JPA/Hibernate > Database
+	// UserDaoService > Static List
+	
+	private static List<User> users = new ArrayList<>();
+
+	private static int usersCount = 0;
+	
+	static {
+		
+		users.add(new User(++usersCount, "Vinicius", LocalDate.now().minusYears(26)));
+		users.add(new User(++usersCount, "Kelly", LocalDate.now().minusYears(25)));
+		users.add(new User(++usersCount, "Nicollas", LocalDate.now().minusYears(16)));
+	}
+	
+	public List<User> findAll() {
+		return users;
+	}
+
+	public User findOne(int id) {
+		Predicate<? super User> predicate = user -> user.getId().equals(id); 
+		return users.stream().filter(predicate).findFirst().orElse(null);
+	}
+	
+	public User save(User user){
+		user.setId(++usersCount);
+		users.add(user);
+		return user;
+	}
+
+	@SuppressWarnings("unlikely-arg-type")
+	public void deleteById(int id) {
+		Predicate<? super User> predicate = user -> user.getId().equals(id);
+		users.remove(predicate);
+	}
+}
